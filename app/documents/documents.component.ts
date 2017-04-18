b2d587d751a35107f5420a4fdc476f5b8ea5b752
Stephanie Pinion <stephpinion@gmail.com>
2017-04-18 17:34:59 -0600

    added documents api service to pull documents

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
-
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: "asdfasdfasdf asdfasdf",
-      file_url: "http://google.com",
-      updated_at: '11/11/16',
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/320px-Mistakes-to-avoid-when-hiring-freelancers.jpg"
-    },
-    {
-      title: "My Second Doc",
-      description: "asdfasdfasdf asdfasdf",
-      file_url: "http://google.com",
-      updated_at: '11/11/16',
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/320px-Mistakes-to-avoid-when-hiring-freelancers.jpg"
-    },
-    {
-      title: "My Last Doc",
-      description: "asdfasdfasdf asdfasdf",
-      file_url: "http://google.com",
-      updated_at: '11/11/16',
-      image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/320px-Mistakes-to-avoid-when-hiring-freelancers.jpg"
-    }
-  ]}
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService,
+  ){}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments(){
+    this.documentService.getDocuments()
+        .subscribe(
+           documents => this.documents = documents,
+           error => this.errorMessage = <any>error
+         );
+  }
+}
