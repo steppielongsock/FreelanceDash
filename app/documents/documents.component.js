6d79f1d97df58b6a4f37256933bfb9bed6b4e38f
Stephanie Pinion <stephpinion@gmail.com>
2017-04-17 21:12:01 -0600

    Initial Commit

---
+++ app/documents/documents.component.js
@@ -0,0 +1,47 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@angular/core");
+let DocumentsComponent = class DocumentsComponent {
+    constructor() {
+        this.pageTitle = "Document Dashboard";
+        this.documents = [
+            {
+                title: "My First Doc",
+                description: "asdfasdfasdf asdfasdf",
+                file_url: "http://google.com",
+                updated_at: '11/11/16',
+                image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/320px-Mistakes-to-avoid-when-hiring-freelancers.jpg"
+            },
+            {
+                title: "My Second Doc",
+                description: "asdfasdfasdf asdfasdf",
+                file_url: "http://google.com",
+                updated_at: '11/11/16',
+                image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/320px-Mistakes-to-avoid-when-hiring-freelancers.jpg"
+            },
+            {
+                title: "My Last Doc",
+                description: "asdfasdfasdf asdfasdf",
+                file_url: "http://google.com",
+                updated_at: '11/11/16',
+                image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/320px-Mistakes-to-avoid-when-hiring-freelancers.jpg"
+            }
+        ];
+    }
+};
+DocumentsComponent = __decorate([
+    core_1.Component({
+        moduleId: module.id,
+        selector: 'documents',
+        templateUrl: 'documents.component.html',
+        styleUrls: ['documents.component.css']
+    })
+], DocumentsComponent);
+exports.DocumentsComponent = DocumentsComponent;
+//# sourceMappingURL=documents.component.js.map
---
+++ app/proposal/proposal-list.component.js
@@ -0,0 +1,31 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@angular/core");
+const proposal_1 = require("./proposal");
+let ProposalListComponent = class ProposalListComponent {
+    constructor() {
+        this.proposalOne = new proposal_1.Proposal(15, 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+        this.proposalTwo = new proposal_1.Proposal(99, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+        this.proposalThree = new proposal_1.Proposal(300, 'Something Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
+    }
+};
+ProposalListComponent = __decorate([
+    core_1.Component({
+        moduleId: module.id,
+        selector: 'proposal-list',
+        templateUrl: 'proposal-list.component.html'
+    })
+], ProposalListComponent);
+exports.ProposalListComponent = ProposalListComponent;
+//# sourceMappingURL=proposal-list.component.js.map
